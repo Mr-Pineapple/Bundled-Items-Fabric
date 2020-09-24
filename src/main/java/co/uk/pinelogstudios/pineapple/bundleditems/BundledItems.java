d5f3e461ae1e223015d38402f32b23d2adb6dc56
Mr. Pineapple <mr.sad.pineapple@gmail.com>
2020-09-25 00:41:33 +0100

    Version 1.4.0
    Updated to 1.4.0 features
    Not added new 1.4.0 items as textures are needed.

---
+++ src/main/java/co/uk/pinelogstudios/pineapple/bundleditems/BundledItems.java
@@ -0,0 +1,26 @@
+package co.uk.pinelogstudios.pineapple.bundleditems;
+
+import co.uk.pinelogstudios.pineapple.bundleditems.init.ItemList;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+/**
+ *	Author:	Mr. Pineapple
+ */
+public class BundledItems implements ModInitializer {
+	
+	public static final String MOD_ID = "bundleditems";
+
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier(MOD_ID, "items"),
+			() -> new ItemStack(ItemList.SECRET_ITEM));
+	
+	@Override
+	public void onInitialize() {
+		ItemList.registerItems();
+	}
+
+}
